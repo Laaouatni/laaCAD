289cc38747f11eef05e911f6b38e88f8b4a12d0f
Laaouatni Anas <laaouatni.anas@outlook.it>
2023-12-19 17:56:53 +0100

    thinking about how to structure the Types in Typescript, before starting the project

---
+++ app/src/lib/Types/example.ts
@@ -0,0 +1,46 @@
+type TypeElementType = "Element" | "Group";
+
+type TypeElementGeometryType = "Point" | "Line" | "Circle";
+
+type TypeCoordinateXYZ = {
+  [axisName in "x" | "y" | "z"]: number;
+};
+
+type TypeElementGeometryDataPosition = {
+  start: TypeCoordinateXYZ;
+  end: TypeCoordinateXYZ;
+};
+
+type TypeElementGeometryData = {
+  position: TypeElementGeometryDataPosition;
+  rotation: TypeCoordinateXYZ;
+  radius?: number;
+};
+
+type TypeProperties = {
+  thickness: number;
+  color: string;
+};
+
+type TypeElement = {
+  type: TypeElementType;
+  geometryType: TypeElementGeometryType;
+  geometryData: TypeElementGeometryData;
+  properties: TypeProperties;
+  elements?: TypeElement[];
+};
+
+type TypeProjects = {
+  [fileName: string]: {
+    elements: TypeElement[]
+  }
+}
+
+type TypeFileStructure = {
+  system: {
+    projects: TypeProjects;
+  },
+  default: {
+    [propertyName in "*" | TypeElementGeometryType]: TypeProperties
+  }
+}
