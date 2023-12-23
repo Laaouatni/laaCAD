4f4f3e14ebc9002ea5c9c6373dfb20ed1f38c32e
Laaouatni Anas <laaouatni.anas@outlook.it>
2023-12-23 18:30:51 +0100

    tailwindcss new config

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config}*/
+
+const colors = require("tailwindcss/colors");
+
+const twReverseColors = (obj) => {
+  const arr = Object.entries(obj);
+  return Object.fromEntries(
+    arr.map(([, value], i) => {
+      const l = arr.length;
+      const reversedIndex = l - 1 - i;
+      const reversedKey = arr[reversedIndex][0];
+      return [reversedKey, value];
+    }),
+  );
+};
+
 const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: colors.neutral,
+        dark: twReverseColors(colors.neutral),
+      },
+    },
   },
 
   plugins: [],
