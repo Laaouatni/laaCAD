7850adaef103e260f819f7f7a04466c3b73c30f1
Laaouatni Anas <laaouatni.anas@outlook.it>
2023-12-23 18:55:17 +0100

    card ui component

---
+++ app/src/lib/Components/UI/UiCard/UiCard.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let twClass: string = "";
+  export {twClass as class};
+</script>
+
+<div class="p-4 bg-light-200 dark:bg-dark-200 rounded-xl shadow-xl shadow-light-300 dark:shadow-dark-300 border border-light-300 dark:border-dark-300 {twClass}">
+  <slot />
+</div>
