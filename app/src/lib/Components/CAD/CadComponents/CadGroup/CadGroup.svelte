70666be11b2a416929c346dbd38b71b925dc8cd5
Laaouatni Anas <laaouatni.anas@outlook.it>
2023-12-25 13:05:56 +0100

    corrected <g> bug... now is not creating a g element for every element, but only for groups

--- app/src/lib/Components/CAD/CadComponents/CadGroup/CadGroup.svelte
+++ app/src/lib/Components/CAD/CadComponents/CadGroup/CadGroup.svelte
@@ -2,16 +2,14 @@
   import type { TypeElementOrGroup } from "$types/TypeCadComponent/TypeCadComponent";
   import CadElement from "../CadElement/CadElement.svelte";
   export let childElements: TypeElementOrGroup[];
-
-  import { getElementProperty } from "$logic/getElementProperty";
 </script>
 
-{#each childElements as thisElementObj, i}
-  <g>
+<g>
+  {#each childElements as thisElementObj, i}
     {#if thisElementObj.type === "CadGroup"}
       <svelte:self childElements={thisElementObj.elements} />
     {:else}
       <CadElement CadElementObj={thisElementObj}></CadElement>
     {/if}
-  </g>
-{/each}
+  {/each}
+</g>
