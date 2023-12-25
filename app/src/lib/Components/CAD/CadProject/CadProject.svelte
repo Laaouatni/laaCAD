40bc4f8fc81832cbd5923101f5118ed310ed7363
Laaouatni Anas <laaouatni.anas@outlook.it>
2023-12-25 13:59:45 +0100

    some progress about scaling, width, positioning... but I still need to get and understand viewport and viewbox because sometimes is overflowing. I will fix it soon

--- app/src/lib/Components/CAD/CadProject/CadProject.svelte
+++ app/src/lib/Components/CAD/CadProject/CadProject.svelte
@@ -4,11 +4,50 @@
 
   export let projectName: string;
 
-  // $: console.log(
-  //   JSON.stringify($appStore.system.projects[projectName].elements, null, 4),
-  // );
+  const positioning = {
+    allAvaiableAxisPositions: {
+      min: "Min",
+      center: "Mid",
+      max: "Max",
+    },
+    choosedPosition: {
+      get x() {
+        return positioning.allAvaiableAxisPositions.min;
+      },
+      get y() {
+        return positioning.allAvaiableAxisPositions.min;
+      },
+    },
+  };
+
+  const viewPort = {
+    x: 400,
+    y: 400,
+  };
+
+  const viewBox = Object.values({
+    start: Object.values({
+      x: 0,
+      y: 0,
+    }).join(" "),
+    end: Object.values({
+      x: 400,
+      y: 400,
+    }).join(" "),
+  }).join(" ");
+
+  const preserveAspectRatio = Object.values({
+    centerViewBoxInsideViewPort: `x${positioning.choosedPosition.x}Y${positioning.choosedPosition.y}`,
+    implementScaling: "meet",
+  }).join(" ");
 </script>
 
-<svg class="border" height="100%" width="100%">
+<svg
+  class="border"
+  width={viewPort.x}
+  height={viewPort.y}
+  {viewBox}
+  {preserveAspectRatio}
+>
   <CadGroup childElements={$appStore.system.projects[projectName].elements} />
 </svg>
